99a664b5ef423f585aebcfc4a459c58c07b34ecf
MateWagner <wwagnermate@gmail.com>
2023-09-27 12:21:17 +0200

    fix: missing annotations

--- src/main/java/com/codecool/carrental/repository/AccountRepository.java
+++ src/main/java/com/codecool/carrental/repository/AccountRepository.java
@@ -2,9 +2,11 @@ package com.codecool.carrental.repository;
 
 import com.codecool.carrental.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByEmail(String username);
 }
--- src/main/java/com/codecool/carrental/repository/CarRepository.java
+++ src/main/java/com/codecool/carrental/repository/CarRepository.java
@@ -2,11 +2,13 @@ package com.codecool.carrental.repository;
 
 import com.codecool.carrental.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
     List<Car> findAllByIdNotInAndIsActive(Set<Long> reservedCarsId, boolean isActive);
 
